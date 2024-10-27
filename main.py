2c20683ccbb434670d6d0279c6e31833597b1629
IDAH-BITBOX <limsejin88@gmail.com>
2024-10-27 19:35:58 +0900

    update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 import httpx
 import os
@@ -11,4 +11,11 @@ templates = Jinja2Templates(directory="templates")
 async def get_map(request: Request, client_id: str, client_secret: str):
     return templates.TemplateResponse("map.html", {"request": request, "client_id": client_id, "client_secret": client_secret})
 
-
+@app.get("/img/{img_fname}")
+async def get_img(img_fname: str):
+    # 이미지 파일이 저장된 디렉토리
+    img_dir = os.path.join("img", img_fname)  # 'images' 폴더 안에 있는 파일을 찾음
+    if os.path.exists(img_dir):
+        return FileResponse(img_dir)  # 이미지 파일 반환
+    else:
+        return {"error": "File not found"}  # 파일이 존재하지 않을 경우 에러 메시지 반환
